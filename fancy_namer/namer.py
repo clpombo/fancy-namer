971b269e40e40413906f1966f52db535ee0b39f5
Carlos Gustavo Lopez Pombo <clpombo@dc.uba.ar>
2026-05-15 13:28:38 -0300

    Arranged imports.

--- fancy_namer/namer.py
+++ fancy_namer/namer.py
@@ -1,10 +1,9 @@
-from fileinput import filename
 import random
 from typing import Optional
 from pathlib import Path
 
-from exceptions import InvalidToolNameError
-from enums import NameType
+from fancy_namer.exceptions import InvalidToolNameError
+from fancy_namer.enums import NameType
 
 
 class FancyNamer:
@@ -16,17 +15,17 @@ class FancyNamer:
         ) -> str:
         if (name_type == NameType.TOOL and not tool_name):
             raise InvalidToolNameError("tool_name must be provided when using NameType.TOOL")
-        adjectives = FancyNamer.load_word_list("adjectives.txt")
+        adjectives = FancyNamer._load_word_list("adjectives.txt")
         match name_type:
             case NameType.STANDARD:
-                nouns = FancyNamer.load_word_list("nouns.txt")
+                nouns = FancyNamer._load_word_list("nouns.txt")
             case NameType.COMPUTER_SCIENCE:
-                nouns = FancyNamer.load_word_list("computer_science_terms.txt")
+                nouns = FancyNamer._load_word_list("computer_science_terms.txt")
             case NameType.TOOL:
                 nouns = [tool_name]  # For TOOL type, the noun is just the tool name
             case _:
                 raise InvalidToolNameError(f"Unsupported name type: {self.name_type}")
-        places = FancyNamer.load_word_list("places.txt")
+        places = FancyNamer._load_word_list("places.txt")
         random_gen = random.Random(random_seed)
         adjective = random_gen.choice(adjectives)
         noun = random_gen.choice(nouns)
@@ -34,7 +33,7 @@ class FancyNamer:
         return f"{adjective}_{noun}_of_{place}"
     
     @staticmethod
-    def load_word_list(filename: str) -> list[str]:
+    def _load_word_list(filename: str) -> list[str]:
         path = (Path(__file__).parent / "data" / filename)
         with path.open() as f:
             word_list = []
